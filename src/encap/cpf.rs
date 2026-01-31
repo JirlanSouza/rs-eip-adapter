22947ca1a702191a8e5278b2e457fd76ae407415
Jirlan Souza <jirlansouza08@gmail.com>
2026-01-31 15:02:50 -0400

    test(encap/cpf): add unit tests for CpfEncoder (counts, lengths, zero-length, prev-length update)

--- src/encap/cpf.rs
+++ src/encap/cpf.rs
@@ -41,7 +41,7 @@ impl<'a> CpfEncoder<'a> {
     pub fn add_item_start(&mut self, item_id: CpfItemId) -> &mut BytesMut {
         self.add_item_end();
         self.buffer.put_u16_le(item_id as u16);
-        
+
         self.current_item_len_pos = Some(self.buffer.len());
         self.buffer.put_u16_le(0);
         self.item_count += 1;
@@ -53,14 +53,110 @@ impl<'a> CpfEncoder<'a> {
         let count_bytes = self.item_count.to_le_bytes();
         self.buffer[self.count_pos..self.count_pos + 2].copy_from_slice(&count_bytes);
     }
-    
+
     fn add_item_end(&mut self) {
         if let Some(len_pos) = self.current_item_len_pos.take() {
             let data_start = len_pos + 2;
             let data_len = (self.buffer.len() - data_start) as u16;
-            
+
             let len_bytes = data_len.to_le_bytes();
             self.buffer[len_pos..len_pos + 2].copy_from_slice(&len_bytes);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bytes::{BufMut, BytesMut};
+
+    #[test]
+    fn cpf_single_item_length_and_count() {
+        let mut buf = BytesMut::new();
+        let mut cpf_encoder = CpfEncoder::new(&mut buf);
+        {
+            let item = cpf_encoder.add_item_start(CpfItemId::IdentityItem);
+            item.put_u8(0xAA);
+            item.put_u8(0xBB);
+        }
+        cpf_encoder.finish();
+
+        let items_count = u16::from_le_bytes([buf[0], buf[1]]);
+        assert_eq!(items_count, 1);
+
+        let item_id = u16::from_le_bytes([buf[2], buf[3]]);
+        assert_eq!(item_id, CpfItemId::IdentityItem as u16);
+
+        let items_len = u16::from_le_bytes([buf[4], buf[5]]);
+        assert_eq!(items_len, 2);
+
+        assert_eq!(buf[6], 0xAA);
+        assert_eq!(buf[7], 0xBB);
+    }
+
+    #[test]
+    fn cpf_multiple_items_and_lengths() {
+        let mut buf = BytesMut::new();
+        let mut cpf_encoder = CpfEncoder::new(&mut buf);
+        {
+            let first_item = cpf_encoder.add_item_start(CpfItemId::IdentityItem);
+            first_item.put_slice(&[1, 2, 3]);
+        }
+        {
+            let second_item = cpf_encoder.add_item_start(CpfItemId::SockAddrInfoOtoT);
+            second_item.put_u8(0xFF);
+        }
+        cpf_encoder.finish();
+
+        let items_count = u16::from_le_bytes([buf[0], buf[1]]);
+        assert_eq!(items_count, 2);
+
+        let first_item_id = u16::from_le_bytes([buf[2], buf[3]]);
+        assert_eq!(first_item_id, CpfItemId::IdentityItem as u16);
+        let first_item_len = u16::from_le_bytes([buf[4], buf[5]]);
+        assert_eq!(first_item_len, 3);
+
+        let second_item_id_offset = 6 + (first_item_len as usize);
+        let second_item_id = u16::from_le_bytes([buf[second_item_id_offset], buf[second_item_id_offset + 1]]);
+        assert_eq!(second_item_id, CpfItemId::SockAddrInfoOtoT as u16);
+        let second_item_len = u16::from_le_bytes([buf[second_item_id_offset + 2], buf[second_item_id_offset + 3]]);
+        assert_eq!(second_item_len, 1);
+        let second_item_data = buf[second_item_id_offset + 4];
+        assert_eq!(second_item_data, 0xFF);
+    }
+
+    #[test]
+    fn cpf_zero_length_item() {
+        let mut buf = BytesMut::new();
+        let mut cpf_encoder = CpfEncoder::new(&mut buf);
+        {
+            let _ = cpf_encoder.add_item_start(CpfItemId::IdentityItem);
+        }
+        cpf_encoder.finish();
+
+        let count = u16::from_le_bytes([buf[0], buf[1]]);
+        assert_eq!(count, 1);
+        let len = u16::from_le_bytes([buf[4], buf[5]]);
+        assert_eq!(len, 0);
+    }
+
+    #[test]
+    fn cpf_add_item_end_updates_previous_length() {
+        let mut buf = BytesMut::new();
+        let mut cpf_encoder = CpfEncoder::new(&mut buf);
+        {
+            let first_item = cpf_encoder.add_item_start(CpfItemId::IdentityItem);
+            first_item.put_slice(&[10, 11]);
+            let second_item = cpf_encoder.add_item_start(CpfItemId::ConnectedAddress);
+            second_item.put_u8(0x01);
+        }
+        cpf_encoder.finish();
+
+        let first_item_len = u16::from_le_bytes([buf[4], buf[5]]);
+        assert_eq!(first_item_len, 2);
+
+        let second_item_len_offset = 6 + 2 + 2;
+        let second_item_len = u16::from_le_bytes([buf[second_item_len_offset], buf[second_item_len_offset + 1]]);
+        assert_eq!(second_item_len, 1);
     }
 }
