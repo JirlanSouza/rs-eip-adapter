f10b70f72c0470a2bd1ef1f116d83ad2fec7616f
Jirlan Souza <jirlansouza08@gmail.com>
2026-02-14 15:07:45 -0400

    Refactor UDP transport: Extract handle_datagram method

--- src/transport/udp.rs
+++ src/transport/udp.rs
@@ -1,7 +1,12 @@
 use crate::encap::{handler::EncapsulationHandler, header::ENCAPSULATION_HEADER_SIZE};
 use bytes::BytesMut;
-use std::{io, net::Ipv4Addr};
+use std::{
+    io,
+    net::{Ipv4Addr, SocketAddr},
+};
 use tokio::{net::UdpSocket, sync::broadcast::Receiver};
+
+const MAX_UDP_DATAGRAM_SIZE: usize = 2048;
 
 pub struct UdpTransport {
     broadcast_socket: UdpSocket,
@@ -25,44 +30,23 @@ impl UdpTransport {
     }
 
     pub async fn listen_broadcast(&self, mut shutdown: Receiver<()>) -> io::Result<()> {
-        log::info!("Listening for UDP broadcast packets on {}", self.broadcast_socket.local_addr()?);
-        let mut receiv_buf = [0u8; 2048];
+        log::info!(
+            "Listening for UDP broadcast packets on {}",
+            self.broadcast_socket.local_addr()?
+        );
+        let mut receiv_buf = [0u8; MAX_UDP_DATAGRAM_SIZE];
         loop {
             tokio::select! {
                 result = self.broadcast_socket.recv_from(&mut receiv_buf) => {
-                    let (len, src) = result?;
-
-                    if len < ENCAPSULATION_HEADER_SIZE {
-                        log::warn!("Received packet with insufficient length from {}", src);
-                        continue;
-                    }
-
-                    log::info!("Received packet with {} bytes from {}", len, src);
-                    let data_buf = BytesMut::from(&receiv_buf[..len]);
-                    let handle_result = self
-                        .encapsulation_handler
-                        .handle_udp_broadcast(data_buf.freeze());
-
-                    if let Some(reply_buf) = handle_result {
-                        log::info!(
-                            "Sending reply to {:?} with {} bytes",
-                            src.ip(),
-                            reply_buf.len()
-                        );
-                        match self.broadcast_socket.send_to(&reply_buf, src).await {
-                            Ok(_) => {
-                                log::info!(
-                                    "Reply {} bytes sent successfully to {:?}",
-                                    reply_buf.len(),
-                                    src
-                                );
-                            }
-                            Err(e) => log::error!("Failed to send reply to {:?}: {}", src.ip(), e),
+                    match result {
+                        Ok((len, src)) => {
+                            self.handle_datagram(BytesMut::from(&receiv_buf[..len]), src).await;
                         }
-                        continue;
+                        Err(err) => {
+                            log::error!("Failed to receive UDP broadcast packet: {}", err);
+                        }
                     }
 
-                    log::info!("No bytes to reply to {:?}", src.ip());
                 }
                 _ = shutdown.recv() => {
                     log::info!("UDP transport shutting down");
@@ -71,5 +55,41 @@ impl UdpTransport {
             }
         }
         Ok(())
+    }
+
+    async fn handle_datagram(&self, data_buf: BytesMut, src: SocketAddr) {
+        if data_buf.len() < ENCAPSULATION_HEADER_SIZE {
+            log::warn!("Received packet with insufficient length from {}", src);
+            return;
+        }
+
+        log::info!("Received packet with {} bytes from {}", data_buf.len(), src);
+
+        let mut handle_result = self
+            .encapsulation_handler
+            .handle_udp_broadcast(data_buf.freeze());
+
+        if handle_result.is_none() {
+            log::info!("No bytes to reply to {:?}", src.ip());
+            return;
+        }
+
+        let reply_buf = handle_result.take().unwrap();
+        log::info!(
+            "Sending reply to {:?} with {} bytes",
+            src.ip(),
+            reply_buf.len()
+        );
+
+        match self.broadcast_socket.send_to(&reply_buf, src).await {
+            Ok(_) => {
+                log::info!(
+                    "Reply {} bytes sent successfully to {:?}",
+                    reply_buf.len(),
+                    src
+                );
+            }
+            Err(e) => log::error!("Failed to send reply to {:?}: {}", src.ip(), e),
+        }
     }
 }
